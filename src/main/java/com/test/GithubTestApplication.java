24332cf0f7197fe3ad3e63fa6348a317a41d4808
minkawoo <minkawoo@gmail.com>
2023-08-07 15:23:59 +0900

    Second

--- src/main/java/com/test/GithubTestApplication.java
+++ src/main/java/com/test/GithubTestApplication.java
@@ -2,6 +2,8 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GithubTestApplication {
@@ -9,5 +11,7 @@ public class GithubTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GithubTestApplication.class, args);
     }
+
+
 
 }
